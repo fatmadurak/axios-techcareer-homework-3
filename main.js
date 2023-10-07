1f3e8f9bbc07abb1791060bddfad19c8909b37a0
fatmadurak <fatmabadillidurak@gmail.com>
2023-10-07 22:24:23 +0300

    filter unitPrice

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const tBody = document.querySelector(".tBody");
-const nameInput = document.querySelector("#nameFilter");
+const nameInput = document.querySelector("#name");
+const nameFilter = document.querySelector("#nameFilter");
 const unitPriceInput = document.querySelector("#unitPrice");
 const stockInput = document.querySelector("#stock");
-
+const selectPrice=document.querySelector("#unitPriceFilter");
 
 
 //APİ URL
@@ -19,6 +20,12 @@ const getProducts = () => {
     .then((res) => {
       productData = res.data;
       tableFill(productData);
+
+      //selectPrice
+      const unitPrice = productData.map((item) => item.unitPrice);
+      selectPrice.innerHTML = unitPrice.map((item) => `
+        <option value="${item}">${item}</option>
+      `).join('');
     })
     .catch((error) => {
       console.error("Veri çekme sırasında bir hata oluştu: ", error);
@@ -48,7 +55,7 @@ const tableFill = (data) => {
       <td>${item.name}</td>
       <td>${item.unitPrice}</td>
       <td>${item.unitsInStock}</td>
-      <td><button class="btnDelete" data-id="${item.id}" onclick="deleteProduct(${item.id})">Sil</button></td>
+      <td><button class="btnDelete" data-id="${item.id}" onclick="deleteProduct(${item.id})">Delete</button></td>
     `;
 
     tBody.appendChild(tr);
@@ -72,7 +79,7 @@ const addNewProduct = () => {
     .post(url, newProduct)
     .then((res) => {
       console.log(res.data);
-      getProducts(); // Ürünleri güncelle
+      getProducts(); // Ürünleri güncelleme
       nameInput.value = "";
       unitPriceInput.value = "";
       stockInput.value = "";
@@ -90,7 +97,7 @@ const deleteProduct = (id) => {
     .delete(`${url}/${id}`)
     .then((res) => {
       console.log(res.data);
-      getProducts(); // Ürünleri güncelle
+      getProducts(); // Ürünleri güncelleme
     })
     .catch((error) => {
       console.error("Ürün silinirken hata oluştu:", error);
@@ -101,10 +108,10 @@ const deleteProduct = (id) => {
 
 //name filter
 
-nameInput.addEventListener("input",function(){
+nameFilter.addEventListener("input",function(){
 
 
-const search=nameInput.value.toLowerCase();
+const search=nameFilter.value.toLowerCase();
 
 const filteredData=productData.filter((item)=>item.name.toLowerCase().includes(search))
 
@@ -115,6 +122,13 @@ tableFill(filteredData)
 
 })
 
+
+selectPrice.addEventListener("change", function () {
+  const selectedPrice = selectPrice.value;
+  const filteredData = productData.filter((item) => item.unitPrice <= selectedPrice);
+
+  tableFill(filteredData);
+});
 
 
 
