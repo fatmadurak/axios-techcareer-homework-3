6d83652e40cd8f37d0595d7001f42ecf61688240
fatmadurak <fatmabadillidurak@gmail.com>
2023-10-07 20:27:31 +0300

    delete productFunc codes

--- main.js
+++ main.js
@@ -1,93 +1,81 @@
+const tBody = document.querySelector(".tBody");
+const nameInput = document.querySelector("#name");
+const unitPriceInput = document.querySelector("#unitPrice");
+const stockInput = document.querySelector("#stock");
 
-
-const tBody=document.querySelector(".tBody");
-
-const nameInput=document.querySelector("#name");
-const unitPriceInput=document.querySelector("#unitPrice");
-const stockInput=document.querySelector("#stock");
-
-
-
-
-const url = "https://northwind.vercel.app/api/products";
-let productData = []; 
+const url = `https://northwind.vercel.app/api/products`;
+let productData = [];
 
 // Ürünleri API'den çekme
 const getProducts = () => {
   axios
     .get(url)
     .then((res) => {
-      // Verileri productData dizisine atama
       productData = res.data;
-      tableFill(productData)
-
-
+      tableFill(productData);
     })
     .catch((error) => {
       console.error("Veri çekme sırasında bir hata oluştu: ", error);
     });
 };
 
-// getProducts fonksiyonunu çağırarak verileri çekme
+// sayfa yüklendiğinde verileri al
 getProducts();
 
+// Tabloyu doldur
+const tableFill = (data) => {
+  tBody.innerHTML = ""; // Tabloyu temizle
 
+  data.forEach((item) => {
+    const tr = document.createElement("tr");
 
-
-
-
-//fillTable
-const tableFill=(data)=>{
-   
-data.map((item)=>{
-    
-const tr =document.createElement("tr");
-
-tr.innerHTML=`
-
+    tr.innerHTML = `
       <td>${item.id}</td>
       <td>${item.name}</td>
       <td>${item.unitPrice}</td>
       <td>${item.unitsInStock}</td>
-      <td>Delete</td>
+      <td><button class="btnDelete" data-id="${item.id}" onclick="deleteProduct(${item.id})">Sil</button></td>
+    `;
 
-`
-tBody.appendChild(tr);
+    tBody.appendChild(tr);
+  });
+};
 
-})
-
-
-
-
-}
-
-
-
-
-
-const addNewProduct=()=>{
-
-    //Eklenecek ürün objesi
-const newProduct={
+// Yeni ürün eklemek
+const addNewProduct = () => {
+  const newProduct = {
     name: nameInput.value,
     unitPrice: unitPriceInput.value,
     stock: stockInput.value,
+  };
 
-}
+  axios
+    .post(url, newProduct)
+    .then((res) => {
+      console.log(res.data);
+      getProducts(); // Ürünleri güncelle
+      nameInput.value = "";
+      unitPriceInput.value = "";
+      stockInput.value = "";
+    })
+    .catch((error) => {
+      console.error("Ürün eklenirken hata oluştu: ", error);
+    });
+};
+
+// Sİlme func
+const deleteProduct = (id) => {
+  axios
+    .delete(`${url}/${id}`)
+    .then((res) => {
+      console.log(res.data);
+      getProducts(); // Ürünleri güncelle
+    })
+    .catch((error) => {
+      console.error("Ürün silinirken hata oluştu:", error);
+    });
+};
 
 
-axios.post(url,newProduct)
-.then(res=>{
-
-     getProducts();
 
 
-}) .catch((error) => {
-    console.error("Veri çekme sırasında bir hata oluştu: ", error);
-  });
-
-
-
-
-
-}
